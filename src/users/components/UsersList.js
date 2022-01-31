64c74e1d206145baa23b360b1b4c3757a658264e
pinazza <seifeddine.elkorichi@hotmail.com>
2022-01-31 12:39:23 +0100

    UsersList Page Component

--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import './UsersList.css'
 
+import UserItem from './UserItem'
+
 const UsersList = (props) => {
-  return <div></div>
+  if (props.items.length === 0) {
+    return (
+      <div className='center'>
+        <h2>No Users Found</h2>
+      </div>
+    )
+  }
+
+  return (
+    <ul>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  )
 }
 
 export default UsersList
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return <div>Users Works</div>
+  const USERS = [
+    {
+      id: '1',
+      name: 'Pinazza',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.izismile.com%2Fimg%2Fimg3%2F20100428%2F640%2Fshe_makes_random_640_03.jpg&f=1&nofb=1',
+      places: 4,
+    },
+  ]
+  return <UsersList items={USERS} />
 }
 
 export default Users
